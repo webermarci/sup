8092cdcde6ae71cdf296968437db6ae35a089a04
Wéber Márton <webermarton@gmail.com>
2026-05-03 16:19:00 +0200

    Add docs for Actor, BaseActor, and NewBaseActor
    Explain Actor.Run semantics (return an error to request a restart,
    nil for a clean exit, panics also trigger a restart), and note that
    setLogger is used by the supervisor to inject a logger. Describe
    BaseActor as embeddable with a no-op logger that the supervisor will
    replace when spawning the actor.

--- actor.go
+++ actor.go
@@ -8,17 +8,20 @@ import (
 
 var noOpLogger = slog.New(slog.NewTextHandler(io.Discard, nil))
 
+// Actor represents a concurrent entity that can be supervised. It has a name and a Run method that executes its logic. The Run method should return an error if the actor needs to be restarted, or nil if it can exit cleanly. Panics will also trigger a restart. The setLogger method is used internally by the supervisor to inject a logger into the actor.
 type Actor interface {
 	Name() string
 	Run(context.Context) error
 	setLogger(*slog.Logger)
 }
 
+// BaseActor provides a simple implementation of the Actor interface with a name and a logger. It can be embedded in other structs to create more complex actors. The Name and Logger methods are safe to call from inside Run(), and the setLogger method is used internally by the supervisor to inject a logger into the actor.
 type BaseActor struct {
 	name   string
 	logger *slog.Logger
 }
 
+// NewBaseActor creates a new BaseActor with the given name. The logger is initialized to a no-op logger and will be set by the supervisor when the actor is spawned.
 func NewBaseActor(name string) *BaseActor {
 	return &BaseActor{
 		name:   name,
